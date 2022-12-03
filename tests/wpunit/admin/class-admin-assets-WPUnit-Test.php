0be88cb213e01a34261f65676c329bf072eef5e1
Brian Henry <BrianHenryIE@gmail.com>
2022-12-02 20:38:10 -0800

    Create class-admin-assets-WPUnit-Test.php

---
+++ tests/wpunit/admin/class-admin-assets-WPUnit-Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BrianHenryIE\WP_Private_Uploads\Admin;
+
+/**
+ * @coversDefaultClass \BrianHenryIE\WP_Private_Uploads\Admin\Admin_Assets
+ */
+class Admin_Assets_WPUnit_Test extends \Codeception\TestCase\WPTestCase {
+
+	/**
+	 * @covers ::register_script
+	 */
+	public function test_register_script(): void {
+
+		$sut = new Admin_Assets();
+
+		$sut->register_script();
+
+		global $wp_scripts;
+
+		$registered = $wp_scripts->query('bh-wp-private-uploads-admin-js');
+
+		$this->assertNotFalse( $registered );
+	}
+}
