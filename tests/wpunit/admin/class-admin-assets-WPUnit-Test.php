1445d2e59397c8c0f18038a36d05f5c4ed4766f8
Brian Henry <BrianHenryIE@gmail.com>
2023-10-31 12:14:39 -0700

    Test registering script for meta box upload dialog

--- tests/wpunit/admin/class-admin-assets-WPUnit-Test.php
+++ tests/wpunit/admin/class-admin-assets-WPUnit-Test.php
@@ -2,6 +2,8 @@
 
 namespace BrianHenryIE\WP_Private_Uploads\Admin;
 
+use BrianHenryIE\WP_Private_Uploads\Private_Uploads_Settings_Interface;
+
 /**
  * @coversDefaultClass \BrianHenryIE\WP_Private_Uploads\Admin\Admin_Assets
  */
@@ -12,13 +14,20 @@ class Admin_Assets_WPUnit_Test extends \Codeception\TestCase\WPTestCase {
 	 */
 	public function test_register_script(): void {
 
-		$sut = new Admin_Assets();
+		$settings = self::makeEmpty(
+			Private_Uploads_Settings_Interface::class,
+			array(
+				'get_plugin_slug' => 'test',
+			)
+		);
+
+		$sut = new Admin_Assets( $settings );
 
 		$sut->register_script();
 
 		global $wp_scripts;
 
-		$registered = $wp_scripts->query( 'bh-wp-private-uploads-admin-js' );
+		$registered = $wp_scripts->query( 'test-private-uploads-media-library-js' );
 
 		$this->assertNotFalse( $registered );
 	}
