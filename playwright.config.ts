f13e2c411b5052169934e620bd2ea50431372d17
Brian Henry <BrianHenryIE@gmail.com>
2026-02-07 14:42:26 -0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,31 +1,42 @@
+/**
+ * External dependencies
+ */
 import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+require( 'dotenv' ).config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+export default defineConfig( {
   testDir: './tests/e2e-pw',
+  testIgnore: '**/helpers-tests/**',
+  // globalSetup: require.resolve( './tests/e2e-pw/config/global-setup' ),
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !! process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // https://playwright.dev/docs/test-timeouts
+  // timeout is 30 seconds by default
+  // expect.timeout is 5 seconds by default
+  timeout: 60_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL:
+      process.env.BASEURL ||
+      process.env.WP_BASE_URL ||
+      'http://localhost:8889',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -35,17 +46,17 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices[ 'Desktop Chrome' ] },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices[ 'Desktop Firefox' ] },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { ...devices[ 'Desktop Safari' ] },
     },
 
     /* Test against mobile viewports. */
@@ -75,4 +86,4 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+} );
