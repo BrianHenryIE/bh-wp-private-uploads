bd52a4282e4411156475ec4684b1aa5bf837b1a1
Brian Henry <BrianHenryIE@gmail.com>
2026-02-11 09:39:11 -0800

    Tidy up.

--- includes/wp-includes/class-cli.php
+++ includes/wp-includes/class-cli.php
@@ -1,7 +1,8 @@
 <?php
 /**
+ * Adds WP CLI commands for downloading files to the private uploads directory.
  *
- *
+ * @package brianhenryie/bh-wp-private-uploads
  */
 
 namespace BrianHenryIE\WP_Private_Uploads\WP_Includes;
@@ -11,23 +12,34 @@ use BrianHenryIE\WP_Private_Uploads\Private_Uploads_Settings_Interface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use WP_CLI;
+use WP_CLI\ExitException;
 
+/**
+ * Implements some of the API via CLI.
+ */
 class CLI {
-
 	use LoggerAwareTrait;
 
-	protected Private_Uploads_Settings_Interface $settings;
-
-	protected API_Interface $api;
-
-	public function __construct( API_Interface $api, Private_Uploads_Settings_Interface $settings, LoggerInterface $logger ) {
+	/**
+	 * Constructor.
+	 *
+	 * @param API_Interface                      $api To perform the actions.
+	 * @param Private_Uploads_Settings_Interface $settings Uses `::get_cli_base()` to configure (or not) the commands.
+	 * @param LoggerInterface                    $logger PSR logger.
+	 */
+	public function __construct(
+		protected API_Interface $api,
+		protected Private_Uploads_Settings_Interface $settings,
+		LoggerInterface $logger
+	) {
 		$this->setLogger( $logger );
-		$this->settings = $settings;
-		$this->api      = $api;
 	}
 
 	/**
+	 * Register the commands with WP CLI.
+	 *
 	 * @hooked cli_init
+	 * @see WP_CLI\Runner::setup_bootstrap_hooks()
 	 */
 	public function register_commands(): void {
 
@@ -37,31 +49,69 @@ class CLI {
 			return;
 		}
 
-		// E.g. `wp plugin-slug download http://example.com/file.txt`.
 		WP_CLI::add_command( "{$cli_base} download", array( $this, 'download_url' ) );
 	}
 
 	/**
-	 * wp plugin-slug download http://example.org/my.pdf
+	 * Download a file from a remote URL to the private uploads directory.
 	 *
-	 * @param string[]             $args
-	 * @param array<string,string> $assoc_args
+	 * ## OPTIONS
+	 *
+	 * <url>
+	 * : The location of the file to download.
+	 *
+	 * [--field=<field>]
+	 * : Display the value of a single field
+	 *
+	 * [--fields=<fields>]
+	 * : Limit the output to specific fields.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - json
+	 *   - yaml
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *   # Download Brian Henry's resume.
+	 *   $ wp cli-base download https://BrianHenry.ie/cv
+	 *
+	 * @param string[]             $args Positional arguments.
+	 * @param array<string,string> $assoc_args Named arguments.
+	 * @throws ExitException On `WP_CLI::error()`.
 	 */
 	public function download_url( array $args, array $assoc_args ): void {
 
 		$url = $args[0];
 
-		// wp_parse_url()
-
-		$filtered_url = filter_var( $url, FILTER_SANITIZE_URL );
+		$filtered_url = sanitize_url( $url );
 
 		if ( $url !== $filtered_url ) {
-			WP_CLI::log( 'Input URL did not filter cleanly.' );
-			return;
+			WP_CLI::error( 'Input URL did not filter cleanly: ' . $filtered_url );
 		}
 
-		WP_CLI::log( 'Beginning download of  ' . $filtered_url ); // TODO: ... print to where??
+		// TODO: Move this into the API class.
+		$head_response = wp_safe_remote_head( $filtered_url, array( 'redirection' => 10 ) );
+		if ( ! is_wp_error( $head_response ) ) {
+			$filtered_url = $head_response['http_response']->get_response_object()->url;
+		}
 
-		$this->api->download_remote_file_to_private_uploads( $filtered_url );
+		WP_CLI::log( 'Beginning download of  ' . $filtered_url );
+
+		// TODO: Return an immutable pojo.
+		// TODO: Add post_id to output.
+		$result = $this->api->download_remote_file_to_private_uploads( $filtered_url );
+
+		WP_CLI\Utils\format_items(
+			$assoc_args['format'] ?: 'table',
+			array( $result ),
+			$assoc_args['field'] ?: $assoc_args['fields'] ?: array_keys( $result )
+		);
 	}
 }
