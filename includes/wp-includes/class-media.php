11cd900991efed2da881c371a9b3bc2ba8ec7588
Brian Henry <BrianHenryIE@gmail.com>
2026-02-10 20:35:35 -0800

    null coalesce rather than isset

--- includes/wp-includes/class-media.php
+++ includes/wp-includes/class-media.php
@@ -101,7 +101,7 @@ class Media {
 					|| wp_verify_nonce( sanitize_key( wp_unslash( $_REQUEST['_wpnonce'] ) ), 'query-attachments' );
 		}
 
-		return isset( $_POST['post_type'] ) && $this->settings->get_post_type_name() === $_POST['post_type'];
+		return $this->settings->get_post_type_name() === sanitize_key( wp_unslash( $_POST['post_type'] ?? '' ) );
 	}
 
 	/**
