77b5278195905b4391593bf2a7a259c6b6b42b2a
Brian Henry <BrianHenryIE@gmail.com>
2026-02-15 16:46:39 -0800

    Add PhpDoc

--- includes/functions.php
+++ includes/functions.php
@@ -8,6 +8,13 @@
 namespace BrianHenryIE\WP_Private_Uploads;
 
 if ( ! function_exists( __NAMESPACE__ . '\\get_plugin_name_from_slug' ) ) {
+	/**
+	 * Given `a-plugin-slug` returns the `Name` header from that plugin directory's main plugin file.
+	 *
+	 * The `Name` is the only required header in a plugin so will never be missing or empty.
+	 *
+	 * @param string $plugin_slug The plugin whose name is needed.
+	 */
 	function get_plugin_name_from_slug( string $plugin_slug ): string {
 
 		require_once constant( 'ABSPATH' ) . 'wp-admin/includes/plugin.php';
@@ -25,7 +32,13 @@ if ( ! function_exists( __NAMESPACE__ . '\\get_plugin_name_from_slug' ) ) {
 
 if ( ! function_exists( __NAMESPACE__ . '\\get_plugin_basename' ) ) {
 	/**
-	 * @param array<string,array{Name:string}> $plugins
+	 * Given `a-plugin-slug` finds the full `a-plugin-slug/a-plugin-slug.php` main plugin file for the
+	 * `wp-content/plugins/a-plugin-slug` directory.
+	 *
+	 * @used-by get_plugin_name_from_slug()
+	 *
+	 * @param array<string,array{Name:string}> $plugins The {@see get_plugins()} array.
+	 * @param string $plugin_slug The plugin slug (directory) we are searching for.
 	 */
 	function get_plugin_basename( array $plugins, string $plugin_slug ): ?string {
 
@@ -40,20 +53,37 @@ if ( ! function_exists( __NAMESPACE__ . '\\get_plugin_basename' ) ) {
 }
 
 if ( ! function_exists( __NAMESPACE__ . '\\str_underscores_to_hyphens' ) ) {
-	function str_underscores_to_hyphens( string $string ): string {
-		return str_replace( '_', '-', $string );
+	/**
+	 * Given "a_plugin_slug" returns "a-plugin-slug".
+	 *
+	 * @param string $snake_string A snake-case string.
+	 */
+	function str_underscores_to_hyphens( string $snake_string ): string {
+		return str_replace( '_', '-', $snake_string );
 	}
 }
 
 if ( ! function_exists( __NAMESPACE__ . '\\str_hyphens_to_underscores' ) ) {
-	function str_hyphens_to_underscores( string $string ): string {
-		return str_replace( '-', '_', $string );
+	/**
+	 * Given "a-plugin-slug" returns "a_plugin_slug".
+	 *
+	 * @param string $kebab_string A kebab-case string.
+	 */
+	function str_hyphens_to_underscores( string $kebab_string ): string {
+		return str_replace( '-', '_', $kebab_string );
 	}
 }
 
 if ( ! function_exists( __NAMESPACE__ . '\\str_underscores_to_title_case' ) ) {
-	function str_underscores_to_title_case( string $string ): string {
-		return ucwords( str_replace( '_', ' ', $string ) );
+	/**
+	 * Given "a_plugin_slug" returns "A Plugin Slug".
+	 *
+	 * NB: does not do 100% perfect title case, e.g. "masters_of_the_universe" returns "Masters Of The Universe".
+	 *
+	 * @param string $snake_string A snake-case string.
+	 */
+	function str_underscores_to_title_case( string $snake_string ): string {
+		return ucwords( str_replace( '_', ' ', $snake_string ) );
 	}
 }
 
