72172c3bc101d47fef5bdedcf1869efec4ae04e1
Brian Henry <BrianHenryIE@gmail.com>
2026-02-20 21:32:36 -0800

    `phpcs` document `File_Upload_Result`

--- includes/api/class-file-upload-result.php
+++ includes/api/class-file-upload-result.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Filepath, URL, MIME, plain object.
+ *
+ * @package brianhenryie/bh-wp-private-uploads
+ */
 
 namespace BrianHenryIE\WP_Private_Uploads\API;
 
+/**
+ * @see wp_handle_upload()
+ * @used-by API::move_file_to_private_uploads()
+ */
 class File_Upload_Result {
 
+	/**
+	 * Constructor
+	 *
+	 * @param string $file Filename of the newly-uploaded file.
+	 * @param string $url URL of the newly-uploaded file.
+	 * @param string $type Mime type of the newly-uploaded file.
+	 */
 	public function __construct(
 		public string $file,
 		public string $url,
