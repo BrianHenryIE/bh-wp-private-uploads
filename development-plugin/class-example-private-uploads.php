dc7a72a70779309f8c03b2324c4d01786a928432
Brian Henry <BrianHenryIE@gmail.com>
2026-02-20 21:52:58 -0800

    PhpDoc

--- development-plugin/class-example-private-uploads.php
+++ development-plugin/class-example-private-uploads.php
@@ -1,18 +1,30 @@
 <?php
 /**
+ * Extend the main plugin API to add experimental functions.
+ *
  * @package    brianhenryie/bh-wp-private-uploads
  */
 
 namespace BrianHenryIE\WP_Private_Uploads_Development_Plugin;
 
+use BrianHenryIE\WP_Private_Uploads\API\API;
 use BrianHenryIE\WP_Private_Uploads\BH_WP_Private_Uploads_Hooks;
 use BrianHenryIE\WP_Private_Uploads\Private_Uploads_Settings_Interface as Settings_Interface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 
-class Example_Private_Uploads extends \BrianHenryIE\WP_Private_Uploads\API\API {
+/**
+ * A test-bed for new functions.
+ */
+class Example_Private_Uploads extends API {
 	use LoggerAwareTrait;
 
+	/**
+	 * Constructor
+	 *
+	 * @param Settings_Interface $settings The configured settings.
+	 * @param LoggerInterface    $logger A PSR logger.
+	 */
 	public function __construct( Settings_Interface $settings, LoggerInterface $logger ) {
 
 		parent::__construct( $settings, $logger );
