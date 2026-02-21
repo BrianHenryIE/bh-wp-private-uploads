0239a0fb01e24d3a4953e6f535445bfcd0612e0f
Brian Henry <BrianHenryIE@gmail.com>
2026-02-20 21:42:11 -0800

    document `Is_Private_Result`

--- includes/api/class-is-private-result.php
+++ includes/api/class-is-private-result.php
@@ -1,11 +1,25 @@
 <?php
+/**
+ * @package brianhenryie/bh-wp-private-uploads
+ */
 
 namespace BrianHenryIE\WP_Private_Uploads\API;
 
 use DateTimeInterface;
 
+/**
+ * @used-by API::check_and_update_is_url_private()
+ */
 class Is_Private_Result {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param string            $url The tested URL.
+	 * @param bool              $is_private The outcome.
+	 * @param int               $http_response_code The specific HTTP code.
+	 * @param DateTimeInterface $last_checked A record of when it was checked.
+	 */
 	public function __construct(
 		public string $url,
 		public bool $is_private,
