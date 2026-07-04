a1ba4b429d034e1aea992616d139d5509102d44e
Brian Henry <BrianHenryIE@gmail.com>
2026-07-03 19:19:48 -0700

    Fix codeception issue with Gherkin (update Codeception)

--- behat.php
+++ behat.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * WP_CLI_TEST_DBTYPE=sqlite vendor/bin/behat -c behat.php
+ *
+ * @package brianhenryie/bh-wp-private-uploads
  */
 
 use Behat\Config\Config;
@@ -12,47 +14,50 @@ require_once __DIR__ . '/vendor-wp-cli/autoload.php';
 
 define( 'WP_CLI_ROOT', __DIR__ . '/vendor-wp-cli/wp-cli/wp-cli' );
 
-include WP_CLI_ROOT . '/php/utils.php';
+require_once WP_CLI_ROOT . '/php/utils.php';
 $utils_path = WP_CLI_ROOT . '/php/utils.php';
 
-return (new Config())
-    ->withProfile((new Profile('default'))
-        ->withSuite((new Suite('default'))
-			->withContexts(\BrianHenryIE\WP_Private_Uploads\FeatureContext::class)
-            ->withPaths('tests/features')))
-//    ->withProfile((new Profile('coverage'))
-//        ->withExtension(new Extension(Extension::class, [
-//            'filter' => [
-//                'include' => [
-//                    'directories' => [
-//                        'includes' => null,
-//                    ],
-//                ],
-//                'exclude' => [
-//                    'directories' => [
-//                        'vendor' => null,
-//                        'tests' => null,
-//                    ],
-//                ],
-//            ],
-//            'reports' => [
-//                'clover' => [
-//                    'target' => 'tests/_output/behat-clover.xml',
-//                ],
-//                'html' => [
-//                    'target' => 'tests/_output/behat-html',
-//                    'lowUpperBound' => 50,
-//                    'highLowerBound' => 90,
-//                ],
-//                'text' => [
-//                    'showColors' => true,
-//                    'showOnlySummary' => false,
-//                ],
-//                'php' => [
-//                    'target' => 'tests/_output/behat-coverage.php',
-//                ],
-//            ],
-//        ]))
-//        ->withSuite((new Suite('default'))
-//            ->withPaths('tests/features')))
-;
+return ( new Config() )
+	->withProfile(
+		( new Profile( 'default' ) )
+		->withSuite(
+			( new Suite( 'default' ) )
+			->withContexts( \BrianHenryIE\WP_Private_Uploads\FeatureContext::class )
+			->withPaths( 'tests/features' )
+		)
+	);
+// ->withProfile((new Profile('coverage'))
+// ->withExtension(new Extension(Extension::class, [
+// 'filter' => [
+// 'include' => [
+// 'directories' => [
+// 'includes' => null,
+// ],
+// ],
+// 'exclude' => [
+// 'directories' => [
+// 'vendor' => null,
+// 'tests' => null,
+// ],
+// ],
+// ],
+// 'reports' => [
+// 'clover' => [
+// 'target' => 'tests/_output/behat-clover.xml',
+// ],
+// 'html' => [
+// 'target' => 'tests/_output/behat-html',
+// 'lowUpperBound' => 50,
+// 'highLowerBound' => 90,
+// ],
+// 'text' => [
+// 'showColors' => true,
+// 'showOnlySummary' => false,
+// ],
+// 'php' => [
+// 'target' => 'tests/_output/behat-coverage.php',
+// ],
+// ],
+// ]))
+// ->withSuite((new Suite('default'))
+// ->withPaths('tests/features')))
